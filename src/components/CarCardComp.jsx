991699423100f83084e79411df575730a9c73323
Lutfi1919 <luthfiamad19@gmail.com>
2026-05-29 12:55:35 +0700

    feat: add more components

---
+++ src/components/CarCardComp.jsx
@@ -0,0 +1,71 @@
+import { GoPeople } from "react-icons/go";
+import { IoArrowForward, IoCarSportOutline } from "react-icons/io5";
+import { PiGasPumpLight } from "react-icons/pi";
+import { TbManualGearbox } from "react-icons/tb";
+import { Link } from "react-router-dom";
+
+export default function CarCardComp({ item, type }) {
+    return(
+        <>
+            {/* <div className="ring ring-inset p-5 rounded-2xl">
+                <img src={item.image} className="w-70 object-cover " alt="car image" />
+                <p>{item.name}</p>
+                <p>{item.type}</p>
+                <p>{item.fuel_type}</p>
+                <p>{item.transmission}</p>
+            </div> */}
+            {
+                type === 'admin' ? 
+                <div className="flex w-86 flex-col overflow-hidden ring-1 hover:-translate-y-0.5 transition-all duration-300 hover:shadow-2xl text-[#222222] ring-[#585858]/10 shadow-lg rounded-2xl">
+                    <img src={item.image} alt="Fleet" className='h-45 object-cover' />
+                    <div className="p-5">
+                        <p className='text-xl capitalize'>{item.name}</p>
+                        <p className="font-light text-sm capitalize">{item.type}</p>
+                        <div className="flex flex-row justify-between mt-5 text-[#585858]">
+                            <div className="text-sm font-light">
+                                <p className='flex items-center'><GoPeople className='me-2'/>{item.passengers} Passengers</p>
+                                <p className='flex items-center capitalize'><PiGasPumpLight className='me-2'/>{item.fuel_type}</p>
+                            </div>
+                            <div className="text-sm font-light">
+                                <p className='flex items-center capitalize'><TbManualGearbox className='me-2'/>{item.transmission}</p>
+                            </div>
+                        </div>
+                        <hr className='text-[#585858]/40 border my-5 mx-3'/>
+                        <div className="flex items-center justify-between mb-3">
+                            <p className=''>Price</p>
+                            <p className='font-bold text-lg'><span className='text-[#85BB65]'>Rp {item.price_per_day.toLocaleString('id-ID')}</span><span className='font-light text-sm ms-1'>/day</span></p>
+                        </div>
+                        <div className="flex justify-end">
+                            <Link to={`/vehicles/${item.id}/edit`} className='hover:ring-1 hover:ring-[#222222] hover:ring-inset hover:bg-transparent transition duration-300 hover:text-[#222222] bg-[#222222] text-white text-sm px-5 py-2 rounded-full flex items-center hover:shadow-lg'>Edit <IoArrowForward className='ms-2'/></Link>
+                        </div>
+                    </div>
+                </div>
+                :
+                <div className="flex w-110 flex-col overflow-hidden ring-1 hover:-translate-y-0.5 transition-all duration-300 hover:shadow-2xl text-[#222222] ring-[#585858]/10 shadow-lg rounded-2xl">
+                    <img src={item.image} alt="Fleet" className='h-55 object-cover' />
+                    <div className="p-5">
+                        <p className='text-xl capitalize'>{item.name}</p>
+                        <p className="font-light text-sm capitalize">{item.type}</p>
+                        <div className="flex flex-row justify-between mt-5 text-[#585858]">
+                            <div className="text-sm font-light">
+                                <p className='flex items-center'><GoPeople className='me-2'/>{item.passengers} Passengers</p>
+                                <p className='flex items-center capitalize'><PiGasPumpLight className='me-2'/>{item.fuel_type}</p>
+                            </div>
+                            <div className="text-sm font-light">
+                                <p className='flex items-center capitalize'><TbManualGearbox className='me-2'/>{item.transmission}</p>
+                            </div>
+                        </div>
+                        <hr className='text-[#585858]/40 border my-5 mx-3'/>
+                        <div className="flex items-center justify-between mb-3">
+                            <p className=''>Price</p>
+                            <p className='font-bold text-lg'><span className='text-[#85BB65]'>Rp {item.price_per_day.toLocaleString('id-ID')}</span><span className='font-light text-sm ms-1'>/day</span></p>
+                        </div>
+                        <div className="flex justify-end">
+                            <Link to={`vehicles/${item.id}`} className='hover:ring-1 hover:ring-[#222222] hover:ring-inset hover:bg-transparent transition duration-300 hover:text-[#222222] bg-[#222222] text-white text-sm px-5 py-2 rounded-full flex items-center hover:shadow-lg'>Rent now <IoArrowForward className='ms-2'/></Link>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
