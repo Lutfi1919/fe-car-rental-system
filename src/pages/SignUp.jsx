83432b7ac9adfa6c71803eec3b0a1895104e6a45
Lutfi1919 <luthfiamad19@gmail.com>
2026-05-31 15:29:43 +0700

    feat: add axios

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,33 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import porsche from '../assets/porsche.jpg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { Register } from "../services/user.service";
 
 export default function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("")
+    const [phoneNum, setPhoneNum] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [error, setError] = useState("");   
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const data = await Register({ name, phoneNum, email, newPassword });
+
+            localStorage.setItem("token", data.data.token);
+
+            navigate("/profile")
+
+        } catch (error) {
+            setError("Gagal Login! pastikan mengisi formulir dengan benar");
+        }
+    }
+
 
     useEffect(() => {
         AOS.init({
@@ -12,6 +35,16 @@ export default function SignUp() {
             easing: 'ease-in-out'
         });
     }, [])
+
+    useEffect(() => {
+        if (error) {
+            const timer = setTimeout(() => {
+                setError("");
+            }, 3000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [error]);
 
     return (
         <>
@@ -30,22 +63,22 @@ export default function SignUp() {
                             <p className='text-3xl mb-3 text-[#222222]'>Create an Account</p>
                             <p className='text-[#585858] font-satoshi font-medium'>Welcome to <span className='italic text-[#363636] me-0.5' style={{fontFamily: "Anton"}}>InstaDrive.</span> - Let's Get Started</p>
                             <hr className='text-[#585858] border-t-2 my-10'/>
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div className="mb-4">
-                                    <label className="block text-[#222222] text-sm font-bold mb-2" for="name">Your name</label>
-                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="name" type="text" placeholder="Luthfi Ahmad" />
+                                    <label className="block text-[#222222] text-sm font-bold mb-2" htmlFor="name">Your name</label>
+                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="name" type="text" placeholder="Luthfi Ahmad" value={name} onChange={(e) => setName(e.target.value)} required />
                                 </div>
                                 <div className="mb-4">
-                                    <label className="block text-[#222222] text-sm font-bold mb-2" for="phone_num">Your phone number</label>
-                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="phone_num" type="number" maxLength={12} placeholder="0878 7522 1858" />
+                                    <label className="block text-[#222222] text-sm font-bold mb-2" htmlFor="phoneNum">Your phone number</label>
+                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="phoneNum" type="number" maxLength={12} placeholder="0878 7522 1858" value={phoneNum} onChange={(e) => setPhoneNum(e.target.value)} required />
                                 </div>
                                 <div className="mb-4">
-                                    <label className="block text-[#222222] text-sm font-bold mb-2" for="email">Your email</label>
-                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="email" type="email" placeholder="instaDrive@email.com" />
+                                    <label className="block text-[#222222] text-sm font-bold mb-2" htmlFor="email">Your email</label>
+                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="email" type="email" placeholder="instaDrive@email.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                                 </div>
                                 <div className="mb-4">
-                                    <label className="block text-[#222222] text-sm font-bold mb-2" for="password">Create new password</label>
-                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="password" type="password" placeholder="••••••••••" />
+                                    <label className="block text-[#222222] text-sm font-bold mb-2" htmlFor="password">Create new password</label>
+                                    <input className="w-full px-3 py-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300" id="password" type="password" placeholder="••••••••••" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} required/>
                                 </div>  
                                 <button type='submit' className="w-full bg-[#222222] text-white py-2 px-4 rounded-lg hover:ring hover:ring-inset hover:ring-[#222222] hover:text-[#222222] hover:bg-transparent transition-all duration-150 cursor-pointer">Create new account</button>
                             </form>
